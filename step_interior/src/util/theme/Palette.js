ef74ab17a94ac179ff651e0a71ed0234bd4a9ce7
WJ <weijiant02@gmail.com>
2024-08-18 21:42:16 +0800

    Style : Add light colour palette
    Scope : src/util/theme/Palette.js

---
+++ step_interior/src/util/theme/Palette.js
@@ -0,0 +1,12 @@
+
+
+export default class Palette{
+    static light = {
+        'primary' : '#FFD600',
+        'secondary' : '#172622',
+        'black' : '#141414',
+        'grey' : '#2C2C2C',
+        'white' : '#FEFEFE',
+        'tertiary' : '#FFFADE',
+    };
+}
