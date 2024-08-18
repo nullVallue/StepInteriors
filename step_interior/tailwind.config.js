2dd04245772a2e391f785593ff95eccb327f5e1d
WJ <weijiant02@gmail.com>
2024-08-18 21:41:06 +0800

    Update tailwind file

--- step_interior/tailwind.config.js
+++ step_interior/tailwind.config.js
@@ -1,8 +1,86 @@
 /** @type {import('tailwindcss').Config} */
+
+import Palette from './src/util/theme/Palette'
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{htm.,js}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        /**------------------------------------------------------------------------
+         *                           Components
+         *------------------------------------------------------------------------**/
+        'navbar-bg' : {
+          DEFAULT : Palette.light['white'],
+        },
+        'navbar-text' : {
+          DEFAULT : Palette.light['grey'],
+        },
+
+
+        /**------------------------------------------------------------------------
+         *                           Home
+         *------------------------------------------------------------------------**/
+        /**--------------------------------------------
+         *               hero
+         *---------------------------------------------**/
+        'landing-hero-text' : {
+          DEFAULT : Palette.light['white'],
+        },
+
+        /**--------------------------------------------
+         *               brief desc
+         *---------------------------------------------**/
+        'landing-desc-bg': {
+          DEFAULT : Palette.light['secondary'],
+        },
+        'landing-desc-text' : {
+          DEFAULT : Palette.light['white'],
+        },
+
+        /**--------------------------------------------
+         *               featured projects
+         *---------------------------------------------**/
+        'landing-projects-bg' : {
+          DEFAULT : Palette.light['white'],
+        },
+        'landing-projects-text' : {
+          DEFAULT : Palette.light['black'],
+        },
+
+        
+        /**--------------------------------------------
+         *              testimonial 
+         *---------------------------------------------**/
+        'landing-testimonial-bg' : {
+          DEFAULT : Palette.light['white'],
+        },
+        'landing-testimonial-text' : {
+          DEFAULT : Palette.light['black'],
+        },
+
+        /**--------------------------------------------
+         *               contact
+         *---------------------------------------------**/
+        'landing-contact-bg' : {
+          DEFAULT : Palette.light['tertiary'],
+        },
+        'landing-contact-text' : {
+          DEFAULT : Palette.light['black'],
+        },
+
+
+
+      },
+      backgroundImage: {
+        'hero-bgimg' : "url(/src/assets/images/hero-bg-img.jpg)",
+      }
+
+    },
   },
   plugins: [],
 }
