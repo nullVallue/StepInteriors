627e9abb9c75a6701e34698ceb19a45c67734a13
WJ <weijiant02@gmail.com>
2024-08-18 21:43:29 +0800

    Add website constants
    Scope :
    - constants/companyInfo.js
    - constants/projects.js

---
+++ step_interior/src/util/constants/companyInfo.js
@@ -0,0 +1,23 @@
+
+export const companyName = "Step Interiors"
+export const companyEmail = "info@stepinteriors.com"
+export const companyPhone = "+6012-345 6789"
+
+
+/**------------------------------------------------------------------------
+ *                           Landing Page
+ *------------------------------------------------------------------------**/
+//* description 
+export const landingDescTitle = "We bring your ideas to life"
+export const landingDescText = `
+    At ${companyName}, we specialise in transforming spaces into beautiful, innovative, and practical environments tailored to your unique style and needs. Our team of experienced designers is dedicated to bringing your vision to life, whether it's a cozy home, a dynamic office, or a chic commercial space. With a keen eye for detail and a commitment to excellence, we ensure every project is a harmonious blend of aesthetics and practicality. Let us be your partner in creating inspiring interiors that reflect your personality and enhance your lifestyle.
+`;
+
+//* testimonial 
+export const testimonialTitle = "What Our Clients Say";
+export const testimonialText = `
+Working with ${companyName} has been a remarkable experience. From the initial consultation to the final reveal, their team demonstrated unparalleled creativity and professionalism. They listened to our ideas and transformed them into a cohesive and stunning design that perfectly matches our style. The attention to detail and dedication to quality were evident in every aspect of the project. Our home now feels like a true reflection of us, thanks to the incredible work of ${companyName}.
+
+${companyName} exceeded all our expectations with their innovative approach to our office redesign. They created a space that is not only visually impressive but also highly functional for our team. The designers were attentive, responsive, and incredibly skilled at maximizing our space. Our employees are more motivated, and clients frequently compliment our new office. We are thrilled with the results and grateful for the exceptional service provided by ${companyName}.
+`;
+export const testimonialPerson = "John Doe, Malaysia";
---
+++ step_interior/src/util/constants/projects.js
@@ -0,0 +1,26 @@
+
+
+export const projects = [
+    {
+        title: "The Dawn Project",
+        thumbnail: {
+            src: require('../../assets/images/projects/TheDawn/TheDawnThumbnail.jpg'),
+            alt: "The Dawn Project Thumbnail Image",
+        },
+    },
+    {
+        title: "Moroccan Pride",
+        thumbnail: {
+            src: require('../../assets/images/projects/MoroccanPride/moroccanPride.jpg'),
+            alt: "Moroccan Pride Thumbnail Image",
+        },
+    },
+    {
+        title: "Fluff Basket",
+        thumbnail: {
+            src: require('../../assets/images/projects/FluffBasket/FluffBasket.jpg'),
+            alt: "Fluff Basket Thumbnail Image",
+        },
+    },
+
+];
